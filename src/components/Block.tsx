47f5e0838bba3804e76ae94ef5ab7d7d211ecb87
Haku <hakudevtw@gmail.com>
2025-01-02 15:05:41 +0800

    feat: add Block element

---
+++ src/components/Block.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import type { PropsWithChildren, ComponentProps } from "react";
+
+interface Props extends ComponentProps<"div"> {}
+
+export default function Block({ children, className, ...props }: PropsWithChildren<Props>) {
+  const isClient = typeof window !== "undefined";
+
+  return (
+    <div
+      title={isClient ? "Client" : "Server"}
+      className={cn({
+        "bg-blue-300": isClient,
+        "bg-red-300": !isClient,
+      })}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
